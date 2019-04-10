82b5e85562bc3f72b6746006e050edf8e87ad960
Milovan <milovan.marjanovic@quantox.com>
2019-04-10 17:06:27 +0200

    add header

--- src/components/pages/main/index.jsx
+++ src/components/pages/main/index.jsx
@@ -1,11 +1,12 @@
 import React, { lazy } from 'react';
 import { Route, Switch, withRouter } from 'react-router-dom';
 import { MAIN_ROUTES } from '@constants/routes';
+import Header from '@components/Header';
 
 const MainPage = ({ match: { path } }) => {
   return (
     <div>
-      <div>Header</div>
+      <Header />
       <div>
         <Switch>
           {MAIN_ROUTES.map(route => (
