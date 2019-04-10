bd5f5980a7ac83f0f1e530a233428a322e9c428c
Milovan <milovan.marjanovic@quantox.com>
2019-04-10 14:29:51 +0200

    add show/hide password text

--- src/components/common/InputField/InputField.jsx
+++ src/components/common/InputField/InputField.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
 
 const InputField = ({ label, type, placeholder, hasError }) => {
+  const [inputType, setInputType] = React.useState(type);
+
   return (
     <div className="input-field__holder">
       <label className="label">
         <span className="label-txt">{label}</span>
         <input
           className={`input-field${hasError ? 'input-error' : ''}`}
-          type={type}
+          type={inputType}
           placeholder={placeholder}
         />
+
         {hasError ? <span className="input-error__sign">!</span> : ''}
+
+        {type === 'password' ? (
+          <div
+            className="password-eye"
+            onClick={() => {
+              setInputType(inputType === 'password' ? 'text' : 'password');
+            }}>
+            <i
+              className={`${
+                inputType === 'password' ? 'fas fa-eye' : 'fas fa-eye-slash'
+              }`}
+            />
+          </div>
+        ) : (
+          ''
+        )}
       </label>
     </div>
   );
