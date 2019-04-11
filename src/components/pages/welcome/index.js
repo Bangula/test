ebbe825d58e50de797d40d20d49f3adacc5f19d1
mrbatocko <stefan.batocanin@quantox.com>
2019-04-11 15:30:20 +0200

    Removed unnecessary Route declaration.

--- src/components/pages/welcome/index.js
+++ src/components/pages/welcome/index.js
@@ -1,7 +1,3 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
 import Welcome from './Welcome';
 
-const WelcomePage = () => <Route path="/welcome" component={Welcome} />;
-
-export default WelcomePage;
+export default Welcome;
