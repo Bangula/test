1ad231a046fc3069bdd3067fb60974c4213c09bd
Milovan <milovan.marjanovic@quantox.com>
2019-04-12 15:42:27 +0200

    add edit-profile & change-password pages

--- src/components/pages/main/my-profile/pages/change-password/ChangePassword.jsx
+++ src/components/pages/main/my-profile/pages/change-password/ChangePassword.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import InputField from '@components/InputField';
+import { Link } from 'react-router-dom';
 
 const ChangePassword = () => {
   return (
@@ -6,7 +8,21 @@ const ChangePassword = () => {
       <div className="profile__title-holder">
         <h1 className="title-primary">change password</h1>
       </div>
-      <div className="profile__content-holder">content</div>
+      <div className="profile__content-holder">
+        <div className="profile__content--left">
+          <InputField label="Old PASSWORD:" type="password" />
+        </div>
+
+        <div className="profile__content--right">
+          <InputField label="New Password:" type="password" />
+          <InputField label="Confirm New Password:" type="password" />
+        </div>
+      </div>
+
+      <div className="my-profile__buttons">
+        <Link className="btn wide dark">cancel</Link>
+        <Link className="btn wide tirques">change password</Link>
+      </div>
     </div>
   );
 };
--- src/components/pages/main/my-profile/pages/edit-profile/EditProfile.jsx
+++ src/components/pages/main/my-profile/pages/edit-profile/EditProfile.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import InputField from '@components/InputField';
+import { Link } from 'react-router-dom';
 
 const EditProfile = () => {
   return (
@@ -6,7 +8,23 @@ const EditProfile = () => {
       <div className="profile__title-holder">
         <h1 className="title-primary">edit profile</h1>
       </div>
-      <div className="profile__content-holder">content</div>
+      <div className="profile__content-holder">
+        <div className="profile__content--left">
+          <InputField label="name:" type="text" />
+          <InputField label="surname:" type="text" />
+          <InputField label="job title:" type="text" />
+        </div>
+
+        <div className="profile__content--right">
+          <InputField label="email:" type="email" />
+          <InputField label="phone number:" type="number" />
+        </div>
+      </div>
+
+      <div className="my-profile__buttons">
+        <Link className="btn wide dark">cancel</Link>
+        <Link className="btn wide tirques">confirm changes</Link>
+      </div>
     </div>
   );
 };
