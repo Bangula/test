daa369cb70c7939cb4c487cf7e1ab90a2f3cf118
alan2207 <alan2207@live.com>
2019-04-15 11:34:52 +0200

    fix profile background

--- src/components/pages/main/pages/profile/common/ProfilePageLayout/ProfilePageLayout.jsx
+++ src/components/pages/main/pages/profile/common/ProfilePageLayout/ProfilePageLayout.jsx
@@ -1,14 +1,8 @@
 import React from 'react';
-import backgroundImage from '@images/profile-page.png';
 
 const ProfilePageLayout = ({ children }) => {
   return (
-    <div
-      className="w-screen bg-contain bg-no-repeat pt-5 profile-page-layout"
-      style={{
-        backgroundImage: `url(${backgroundImage})`,
-        height: `calc(100vh - 186px)`,
-      }}>
+    <div className="profile-page-layout">
       <div className="container">
         <div className="profile-content__holder">
           <div className="profile-content">{children}</div>
