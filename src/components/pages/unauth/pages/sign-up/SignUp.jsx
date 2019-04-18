121e52184e9e0fe704b372d8d7e8a476b0d3b1aa
alan2207 <alan2207@live.com>
2019-04-18 16:04:09 +0200

    handle market selection

--- src/components/pages/unauth/pages/sign-up/SignUp.jsx
+++ src/components/pages/unauth/pages/sign-up/SignUp.jsx
@@ -7,6 +7,7 @@ import { connect } from 'react-redux';
 import http from '@services/http';
 import * as actions from '@state/actions';
 import InputField from '@components/InputField/InputField';
+import SelectField from '@components/SelectField/SelectField';
 import ConfirmationSent from '../../common/confirmation-sent/ConfirmationSent';
 import ConfirmationResent from '../../common/confirmation-resent/ConfirmationResent';
 
@@ -67,104 +68,124 @@ const SignUp = ({ match: { url }, ...props }) => {
       }}
       validationSchema={SignUpSchema}
       onSubmit={props.register}>
-      {({ errors, touched }) => (
-        <div className="sign-up">
-          <h1 className="title-primary">Sign Up</h1>
-          <p className="title-subtext">
-            Enter your details to sign up to Axe's <br /> Passions Portal.
-          </p>
+      {({ errors, touched, setFieldValue }) => {
+        return (
+          <div className="sign-up">
+            <h1 className="title-primary">Sign Up</h1>
+            <p className="title-subtext">
+              Enter your details to sign up to Axe's <br /> Passions Portal.
+            </p>
 
-          <div className="form-holder">
-            <Form>
-              <Field
-                name="name"
-                type="text"
-                render={({ field }) => (
-                  <InputField
-                    {...field}
-                    type="text"
-                    label="name"
-                    placeholder="name"
-                    hasError={touched.name && errors.name}
-                  />
-                )}
-              />
-              <Field
-                name="surname"
-                type="text"
-                render={({ field }) => (
-                  <InputField
-                    {...field}
-                    type="text"
-                    label="surname"
-                    placeholder="surname"
-                    hasError={touched.surname && errors.surname}
-                  />
-                )}
-              />
-              <Field
-                name="email"
-                type="email"
-                render={({ field }) => (
-                  <InputField
-                    {...field}
-                    type="email"
-                    label="email"
-                    placeholder="example@axe.com"
-                    hasError={touched.email && errors.email}
-                  />
-                )}
-              />
+            <div className="form-holder">
+              <Form>
+                <Field
+                  name="name"
+                  type="text"
+                  render={({ field }) => (
+                    <InputField
+                      {...field}
+                      type="text"
+                      label="name"
+                      placeholder="name"
+                      hasError={touched.name && errors.name}
+                    />
+                  )}
+                />
+                <Field
+                  name="surname"
+                  type="text"
+                  render={({ field }) => (
+                    <InputField
+                      {...field}
+                      type="text"
+                      label="surname"
+                      placeholder="surname"
+                      hasError={touched.surname && errors.surname}
+                    />
+                  )}
+                />
+                <Field
+                  name="email"
+                  type="email"
+                  render={({ field }) => (
+                    <InputField
+                      {...field}
+                      type="email"
+                      label="email"
+                      placeholder="example@axe.com"
+                      hasError={touched.email && errors.email}
+                    />
+                  )}
+                />
 
-              <Field
-                name="phone"
-                type="number"
-                render={({ field }) => (
-                  <InputField
-                    {...field}
-                    type="number"
-                    label="phone number"
-                    placeholder="+44 (0) 0000 000 000"
-                    hasError={touched.phone && errors.phone}
-                  />
-                )}
-              />
+                <Field
+                  name="phone"
+                  type="number"
+                  render={({ field }) => (
+                    <InputField
+                      {...field}
+                      type="number"
+                      label="phone number"
+                      placeholder="+44 (0) 0000 000 000"
+                      hasError={touched.phone && errors.phone}
+                    />
+                  )}
+                />
 
-              <Field component="select" name="market_id">
-                {markets.map(market => (
-                  <option key={market.id} value={market.id}>
-                    {market.name}
-                  </option>
-                ))}
-              </Field>
+                {/* <Field
+                  component="select"
+                  name="market_id"
+                  render={({ field }) => (
+                    <SelectField
+                      {...field}
+                      label="markets"
+                      placeholder="markets"
+                      hasError={touched.market_id && errors.market_id}
+                    />
+                  )}> */}
+                <SelectField
+                  options={markets}
+                  setFieldValue={setFieldValue}
+                  label="markets"
+                  placeholder="markets"
+                  name="market_id"
+                  hasError={touched.market_id && errors.market_id}
+                />
+                {/* {markets.map(market => (
+                    <option key={market.id} value={market.id}>
+                      {market.name}
+                    </option>
+                  ))} */}
+                {/* </Field> */}
 
-              <Field
-                name="password"
-                type="password"
-                render={({ field }) => (
-                  <InputField
-                    {...field}
-                    type="password"
-                    label="password"
-                    placeholder="password"
-                    hasError={touched.password && errors.password}
-                  />
-                )}
-              />
-              <button type="submit" className="btn">
-                sign up
-              </button>
-            </Form>
+                <Field
+                  name="password"
+                  type="password"
+                  render={({ field }) => (
+                    <InputField
+                      {...field}
+                      type="password"
+                      label="password"
+                      placeholder="password"
+                      hasError={touched.password && errors.password}
+                    />
+                  )}
+                />
+                <button type="submit" className="btn">
+                  sign up
+                </button>
+              </Form>
+            </div>
+
+            <p className="title-subtext confirmation">
+              Already got an account?&nbsp;
+              <Link to={`/`} className="auth-link-text">
+                Sign In
+              </Link>
+            </p>
           </div>
-
-          <p className="title-subtext confirmation">
-            Already got an account?&nbsp;
-            <Link to={`/`} className="auth-link-text">
-              Sign In
-            </Link>
-          </p>
-        </div>
-      )}
+        );
+      }}
     </Formik>
   ) : props.confirmationEmailResent ? (
     <ConfirmationResent />
