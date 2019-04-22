aa6cb3b145a7f18a40b4fd406893f8d2f2d03eb4
mrbatocko <stefan.batocanin@quantox.com>
2019-04-22 16:04:24 +0200

    Do not use class in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import Pages from '@pages';
@@ -6,18 +6,14 @@ import Pages from '@pages';
 import store from './state/store';
 
 import history from '@/services/history';
-class App extends Component {
-  render() {
-    return (
-      <div className="font-bebas">
-        <Provider store={store}>
-          <Router history={history}>
-            <Pages />
-          </Router>
-        </Provider>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="font-bebas">
+    <Provider store={store}>
+      <Router history={history}>
+        <Pages />
+      </Router>
+    </Provider>
+  </div>
+);
 
 export default App;
