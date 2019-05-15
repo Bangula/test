01488992f54400f98f95cdef7f9a2bbc61e517f2
mrbatocko <stefan.batocanin@quantox.com>
2019-05-15 11:04:22 +0200

    Use MediaLibrary in AssetHub.

--- src/components/pages/main/common/Footer/Footer.jsx
+++ src/components/pages/main/common/Footer/Footer.jsx
@@ -15,27 +15,37 @@ const Footer = ({ match: { url } }) => {
         </div>
         <ul className="flex items-center">
           <li className="mr-5">
-            <Link className="font-bebas text-md font-thin uppercase text-white">
+            <Link
+              className="font-bebas text-md font-thin uppercase text-white"
+              to={`${url}`}>
               Website Guide
             </Link>
           </li>
           <li className="mr-5">
-            <Link className="font-bebas text-md font-thin uppercase text-white">
+            <Link
+              className="font-bebas text-md font-thin uppercase text-white"
+              to={`${url}`}>
               Noticeboard
             </Link>
           </li>
           <li className="mr-5">
-            <Link className="font-bebas text-md font-thin uppercase text-white">
+            <Link
+              className="font-bebas text-md font-thin uppercase text-white"
+              to={`${url}`}>
               Sitemap
             </Link>
           </li>
           <li className="mr-5">
-            <Link className="font-bebas text-md font-thin uppercase text-white">
+            <Link
+              className="font-bebas text-md font-thin uppercase text-white"
+              to={`${url}`}>
               Cookie policy
             </Link>
           </li>
           <li>
-            <Link className="font-bebas text-md font-thin uppercase text-white">
+            <Link
+              className="font-bebas text-md font-thin uppercase text-white"
+              to={`${url}`}>
               Contact us
             </Link>
           </li>
@@ -43,22 +53,22 @@ const Footer = ({ match: { url } }) => {
       </div>
       <ul className="flex items-center">
         <li className="pr-5">
-          <Link>
+          <Link to={`${url}`}>
             <i className="fab fa-facebook-f text-white" />
           </Link>
         </li>
         <li className="ml-5 pr-5">
-          <Link>
+          <Link to={`${url}`}>
             <i className="fab fa-twitter text-white" />
           </Link>
         </li>
         <li className="ml-5 pr-5">
-          <Link>
+          <Link to={`${url}`}>
             <i className="fab fa-instagram text-white" />
           </Link>
         </li>
         <li className="ml-5">
-          <Link>
+          <Link to={`${url}`}>
             <i className="fab fa-youtube text-white" />
           </Link>
         </li>
--- src/components/pages/main/pages/artists/index.jsx
+++ src/components/pages/main/pages/artists/index.jsx
@@ -3,7 +3,6 @@ import { Switch, Route } from 'react-router-dom';
 import ArtistsList from './pages';
 import Artist from './pages/artist';
 import Requests from './pages/requests';
-import AssetHub from './pages/asset-hub';
 import EventRequest from './pages/event-request';
 import ExperienceRequest from './pages/experience-request';
 import MerchandiseRequest from './pages/merchandise-request';
@@ -30,7 +29,6 @@ const Artists = ({ match: { path } }) => {
           path={`${path}/merchandise-request`}
           component={MerchandiseRequest}
         />
-        <Route path={`${path}/:artist/asset-hub`} component={AssetHub} />
         <Route path={`${path}/:artist/requests`} component={Requests} />
         <Route path={`${path}/:artist`} component={Artist} />
         <Route exact path={`${path}/`} component={ArtistsList} />
