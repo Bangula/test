799d07dde9f0291d89f777620324d236380bf19f
mrbatocko <stefan.batocanin@quantox.com>
2019-05-17 12:25:17 +0200

    Include additional data to endpoint.

--- src/services/http/endpoints/requests/index.js
+++ src/services/http/endpoints/requests/index.js
@@ -2,5 +2,7 @@ import http from '@services/http';
 import toResponse from '@helpers/to-response';
 
 export const getRequests = () => {
-  return toResponse(http.get('/requests'));
+  return toResponse(
+    http.get('/requests', { params: { include: 'relatesTo' } }),
+  );
 };
